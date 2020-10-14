a8cf7f2a80b1fa3474f04bbf2286055e48c05b0f
home <akshay.cy2@gmail.com>
2020-10-14 12:42:05 -0400

    ~20 warnings fix

--- RecomSys/RecomSys.c
+++ RecomSys/RecomSys.c
@@ -27,12 +27,10 @@ int main()
 
     char firstName[20];
 
-    char artForm[20];
-    int artYear;
-
     char todaysSpecial[20];
     
-    char task;
+    char* task;
+    char artForm[20];
     char artName[75];
 
     //know the user
@@ -45,30 +43,26 @@ int main()
     printf("Hello %s,\n\ndo you like to add your recent fav. movie or search for some movie randomly from available obscure movies\n[for e.g. key: task\n c: to create a new art\nf: to find movie/music/podcast,\nu: to update already present art,\nd: to delete prev. added art from", firstName);
 
     //taking input for task 
-    getf("choose the option key [c / f / u / d]: %c", task);
+    scanf("choose the option key [c / f / u / d]: %c", task);
 
     if (task == "c")
     {
         printf("What form of art do you wish to consume today [enter below movie, music, etc]?: ");
-        scanf("%s", artForm[20]);
+        scanf("%s", artForm);
 
         if (artForm == "movie")
         {
             struct ArtForm movie;
-            artName[75] = movie.name;
-            artYear = movie.releaseYear;
         }
-        else if (artForm[20] == "music")
+        else if (artForm == "music")
         {
             struct ArtForm music;
-            artName[75] = music.name;
         }
         else
         {
             struct ArtForm others;
-            artName[75] = others.name;
         }
-        printf("That\'s interesting,\ndo you like to specify the name of the %s:\n", artForm[20]);
+        printf("That\'s interesting,\ndo you like to specify the name of the %s:\n", artForm);
         scanf("%s", artForm);
     }
     return 0;
