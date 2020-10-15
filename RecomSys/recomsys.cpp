725eb2c8002025459d32fd47b0e29f2e9a8e9b28
home <akshay.cy2@gmail.com>
2020-10-15 15:13:03 -0400

    added few code line to build more features in future

--- RecomSys/recomsys.cpp
+++ RecomSys/recomsys.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 
 /********************************************
  * 
@@ -22,26 +23,32 @@ class Artform {
 int main()
 {
     //inputs variables
-    std::string firstName;
+    std::string userName;
     std::string todaysSpecial;
     
     std::string task;
     std::string artForm;
     std::string artName;
 
+    std::list<std::string> artLibrary{};
+
+
+
     //know the user
 
     std::cout << "Please enter your name:\n";
-    std::cin >> firstName;
+    getline(std::cin, userName);
 
     //Mr. AFRSys greets user
 
-    std::cout << "\nHello " << firstName << ",\n\ndo you like to add your recent fav. movie or search for some movie randomly from available obscure movies\n[for e.g. key: task\nc: to create a new art\nf: to find movie/music/podcast,\nu: to update already present art,\nd: to delete prev. added art form\nchoose the option key [c / f / u / d]:\n";
+    std::cout << "\nHello " << userName << ",\n\ndo you like to add your recent fav. movie or search for some movie randomly from available obscure movies\n[for e.g. key: task\nc: to create a new art\nf: to find movie/music/podcast,\nu: to update already present art,\nd: to delete prev. added art form\nchoose the option key : [c / f / u / d]\n";
     std::cin >> task;
+
+    //user create new category of art
 
     if (task == "c")
     {
-        std::cout << "\nWhat form of art do you wish to create today ? [ enter below : movie, music, etc ] :\n";
+        std::cout << "\nWhat form of art do you wish to create today ? : [for e.g. movie, music, etc]\n";
         std::cin >> artForm;
 
         if (artForm == "movie")
@@ -49,7 +56,9 @@ int main()
             class Artform movie;
             std::cout << "That\'s interesting,\ndo you like to specify the name of the " + artForm + ":\n";
             std::cin >> movie.name;
-            std::cout << "You added new " + movie.name + "to the library\n";
+            std::cout << "You added new " + movie.name + "to the library\nlets add few more info regarding " + movie.name + ", add the release year:\n";
+            
+            std::cin >> movie.releaseYear;
         }
         if (artForm == "music")
         {
@@ -66,6 +75,24 @@ int main()
             std::cout << "You added new " + others.name + "to the library\n";
         }
     }
+
+    if (task == "f")
+    {
+        std::cout << "Please enter the keyword you want to search from library: [for e.g., the big lebowski]\n";
+    }
+
+
+    //show current art library
+    
+    if(artLibrary.size() == 0)
+    {
+        std::cout << "It's a ghost town ={ in here\n";
+    }
+        else
+    {
+        std::cout << "Your art library has item(s) loaded\n";
+    }
+
 
     return 0;
 }
