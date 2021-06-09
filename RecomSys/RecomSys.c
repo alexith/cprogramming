904b314e8d52aeb097c68b4b91184b3e3f2e6216
Akshay Chaudhary <64261194+alexith@users.noreply.github.com>
2021-06-09 11:35:21 +0530

    update RecomSys.c

--- RecomSys/RecomSys.c
+++ RecomSys/RecomSys.c
@@ -1,12 +1,12 @@
 /********************************************
- * 
- * 
- *      ART_FORM RECOMMENDATION SYSTEM
- *          (Mr. AFRSys)
- *            by alexith
- * 
- * 
- *******************************************/
+*
+*
+*      ART_FORM RECOMMENDATION SYSTEM
+*          (Mr. AFRSys)
+*            by alexith
+*
+*
+*******************************************/
 
 
 
@@ -25,15 +25,14 @@ struct ArtForm
     float imdbRating;
 };
 
-int main()
-{
+int main() {
 
     //inputs variables
 
     char firstName[20];
 
     char todaysSpecial[20];
-    
+
     const char* task;
     char artForm[50];
     char artName[50];
@@ -46,23 +45,20 @@ int main()
     //Mr. AFRSys greets user
 
     printf("\nHello %s,\n\ndo you like to add your recent fav. movie or search for some movie randomly from available obscure movies\n[for e.g. key: task\nc: to create a new art\nf: to find movie/music/podcast,\nu: to update already present art,\nd: to delete prev. added art form\n", firstName);
-    //taking input for task 
+    //taking input for task
     scanf("choose the option key [c / f / u / d]: %c", &task);
 
-    if ((task == 'c')
-    {
+    if ((task == 'c') {
         printf("\nWhat form of art do you wish to create today ? [ enter below : movie, music, etc ] :\n");
         scanf("%s", &artForm);
 
-        if (strncmp(artForm, "movie") == "0")
-        {
+        if (strncmp(artForm, "movie") == "0") {
             struct ArtForm movie;
             printf("That\'s interesting,\ndo you like to specify the name of the %s:\n", artForm);
             artName[20] = scanf("%s", &movie.name);
             printf("You added new %s to the library\n", artName);
         }
-        else if (scanf("%s", &artForm) == "music")
-        {
+        else if (scanf("%s", &artForm) == "music") {
             struct ArtForm music;
             printf("That\'s interesting,\ndo you like to specify the name of the %s:\n", artForm);
             artName[20] = scanf("%s", &music.name);
@@ -78,5 +74,5 @@ int main()
 
     }
 
-    return 0;
-}
+        return 0;
+    }
